ef95cc237a95fd58fc368afd4da161594bbee7f7
Subodh <subodhbhowate251@gmail.com>
2023-08-05 00:17:14 +0530

    All code pushed

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <MainHeader style={{height: "200px" }}>
+      <NavLink to="/">
+        <img src="./images/logo.png" alt="my logo img" />
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 5rem;
+  }
+`;
+export default Header;
